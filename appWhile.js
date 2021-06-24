4c79c823ca1e53c41f82bab1d09f2a9890c2fda5
florian <martinflorian2@gmail.com>
2021-06-24 16:53:37 +0200

    range correction

--- appWhile.js
+++ appWhile.js
@@ -10,44 +10,28 @@ const rl = readline.createInterface({
 let min = 1;
 let max = 100;
 
-const getRandomNumber = (min, max) => {
-    return Math.floor(Math.random(min + max) * max - min);
-}
-
 let makeGuess = (min, max) => {
     return Math.floor((max+min) / 2);
 }
 
-const target = getRandomNumber(min, max);
 let guess = makeGuess(min, max);
-console.log(min);
-console.log(max);
-let answer = 'to';
-const askQuestion = (message) => {
-    rl.question(message);
-};
 
 rl.question(`Can you think about a number ?\nIs your number ${guess} \n`, (answer) => {
 
     while (answer != '=') {
         if (answer === '+') {
 
-            min = guess;
+            min = guess++;
             guess = makeGuess(min, max);
             rl.setPrompt(`ok it's more... Is your number ${guess}\n`);
             rl.prompt();
-            // rl.question(`ok it's more... Is your number ${guess}\n`, answer) => {
-                
-            // }
-
         }
         else if (answer === '-') {
 
-            max = guess;
+            max = guess--;
             guess = makeGuess(min, max);
             rl.setPrompt(`ok it's less... Is your number ${guess}\n`);
             rl.prompt();
-
         }
         else {
             rl.setPrompt(`You must use the symbole +, - or = to help the computer find your number. Please try again.\n`);
